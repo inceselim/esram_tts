6be13fad710f9bac4270b79b96672caaa5f61327
inceselim <inceselim91@gmail.com>
2024-08-09 21:28:46 +0300

    ++

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSpeech } from "react-text-to-speech";
+import { useSpeech, useVoices } from "react-text-to-speech";
 import { useState } from 'react';
 
 function App() {
+  const { languages, voices } = useVoices();
+  const [lang, setLang] = useState("");
+  const [voiceURI, setVoiceURI] = useState("");
+
   const [text, setText] = useState("Seni çok seviyorum aşkım başarılar dilerim hep mutlu olasın")
   const [highlightText, setHighlightText] = useState(true);
   const {
@@ -15,11 +19,45 @@ function App() {
     stop, // Function to stop the speech or remove it from queue
   } = useSpeech({
     text: text, highlightText,
-    highlightProps: { style: { color: "white", backgroundColor: "blue" } },
+    volume: 1,
+    rate: 1,
+    highlightProps: { style: { color: "white", backgroundColor: "blue" } }
+    , lang, voiceURI,
   });
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <label htmlFor="lang">Dil: </label>
+          <select
+            id="lang"
+            value={lang}
+            onChange={(e) => {
+              setLang(e.target.value);
+              setVoiceURI("");
+            }}
+          >
+            <option value="">Dil Seç</option>
+            {languages.map((lang) => (
+              <option key={lang} value={lang}>
+                {lang}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="voice">Konuşmacı: </label>
+          <select id="voice" value={voiceURI} onChange={(e) => setVoiceURI(e.target.value)}>
+            <option value="">Konuşmacı Seç</option>
+            {voices
+              .filter((voice) => !lang || voice.lang === lang)
+              .map(({ voiceURI }) => (
+                <option key={voiceURI} value={voiceURI}>
+                  {voiceURI}
+                </option>
+              ))}
+          </select>
+        </div>
         <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
           <Text />
           <div style={{ display: "flex", columnGap: "0.5rem" }}>
