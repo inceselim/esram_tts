129190be078529b1ab235459c4384ee586450658
inceselim <inceselim91@gmail.com>
2024-08-09 21:22:02 +0300

    first commit tts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSpeech } from "react-text-to-speech";
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("Seni çok seviyorum aşkım başarılar dilerim hep mutlu olasın")
+  const [highlightText, setHighlightText] = useState(true);
+  const {
+    Text, // Component that returns the modified text property
+    speechStatus, // String that stores current speech status
+    isInQueue, // Boolean that stores whether a speech utterance is either being spoken or present in queue
+    start, // Function to start the speech or put it in queue
+    pause, // Function to pause the speech
+    stop, // Function to stop the speech or remove it from queue
+  } = useSpeech({
+    text: text, highlightText,
+    highlightProps: { style: { color: "white", backgroundColor: "blue" } },
+  });
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
+          <Text />
+          <div style={{ display: "flex", columnGap: "0.5rem" }}>
+            {speechStatus !== "started" ? <button onClick={start}>Start</button> : <button onClick={pause}>Pause</button>}
+            <button onClick={stop}>Stop</button>
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
